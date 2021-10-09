d830b16d256ff3a6d259a5c4fc66eacb444f86dd
eliseevz <71176626+eliseevz@users.noreply.github.com>
2021-10-09 12:57:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Button from "./components/UI/button/Button";
 
 function App() {
   return (
     <div className="App">
-      <Button action={() => console.log("Присоединиться к команд")} type="primary">
-          Присоединиться к команде
-      </Button>
     </div>
   )
 }
