54bfd55547ed0ab30c0a7c900e5e41093ddd2ba7
eliseevz <71176626+eliseevz@users.noreply.github.com>
2021-10-09 13:35:48 +0300

    Update API.js

--- src/API/API.js
+++ src/API/API.js
@@ -1,29 +1,5 @@
 export const allMembers = [
   {
-    id: 0,
-    isFavorite: false,
-    role: [
-      { name: 'team lead', type: 'danger' },
-      { name: 'developer', type: 'warning' }
-    ],
-    name: 'Eliseev Ivan',
-    age: 20,
-    county: 'Russia',
-    about: 'lorem',
-    impact: 'lorem',
-    stack: [
-      { name: 'HTML', prog: 20 },
-      { name: 'CSS', prog: 100 }
-    ],
-    socialMedia: [
-      { name: 'instagram', link: 'https:/....' },
-      { name: 'telegram', link: 'https:/....' },
-      { name: 'github', link: 'https:/....' },
-      { name: 'linkedin', link: 'https:/....' }
-    ],
-    imageURL: '...'
-  },
-  {
     id: 1,
     isFavorite: false,
     role: [{ name: 'developer', type: 'warning' }],
