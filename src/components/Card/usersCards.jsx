319e59e458266dc98a4c84ba4b1a419a6e74b957
VasilievPavelP <pavell.vasiliev@gmail.com>
2021-10-10 16:12:08 +0300

    add favorite page

--- src/components/Card/usersCards.jsx
+++ src/components/Card/usersCards.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
-import { allMembers } from '../../API/API';
-import UserCard from './userCard';
+import React, { useState } from 'react'
+import { allMembers } from '../../API/API'
+import UserCard from './userCard'
 
 const UsersCards = () => {
-	const [users, setUsers] = useState(allMembers);
+  const [users] = useState(allMembers)
 
-	return (
-		<div className='container p-0'>
-			<h2 className='mb-4'>Kоманда</h2>
-			<div className='row justify-content-center'>
-				{users.map(user => (
-					<UserCard {...user} key={user.id} />
-				))}
-			</div>
-		</div>
-	);
-};
+  return (
+    <div className='container p-0'>
+      <h2 className='mb-4'>Kоманда</h2>
+      <div className='row justify-content-center'>
+        {users.map((user) => (
+          <div key={user.id} className='col-sm-12 col-md-6 col-lg-6 col-xl-3 mb-1'>
+            <UserCard {...user} key={user.id} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
-export default UsersCards;
+export default UsersCards
--- src/pages/members.jsx
+++ src/pages/members.jsx
@@ -1,48 +1,40 @@
-
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import { allMembers } from "../API/API";
-import Badge from "../components/Badge";
-import Progress from "../components/progress/progress";
-import classes from "./members.module.css";
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { allMembers } from '../API/API'
+import Badge from '../components/UI/badge/badge'
+import Progress from '../components/progress/progress'
+import classes from './members.module.css'
 
 const Members = () => {
-  const [userChoose, setUserChoose] = useState(allMembers);
-  const params = useParams();
-  const memberId = params.memberId;
+  const [userChoose, setUserChoose] = useState(allMembers)
+  const params = useParams()
+  const memberId = params.memberId
   if (memberId) {
     return (
-      <div className="container d-flex flex-column pt-5">
-        <div className="d-flex align-items-center">
+      <div className='container d-flex flex-column pt-5'>
+        <div className='d-flex align-items-center'>
           <h1>{userChoose[memberId].name}</h1>
-          <div className="d-flex flex-row">
+          <div className='d-flex flex-row'>
             {userChoose[memberId].role.map((role) => (
               <span>
                 <Badge color={role.type} content={role.name} />
               </span>
             ))}
           </div>
-          <i class="bi bi-heart" style={{ fontSize: "20px" }}></i>
+          <i class='bi bi-heart' style={{ fontSize: '20px' }}></i>
         </div>
-        <div className="d-flex">
-          <div className="d-flex flex-column">
-            <img
-              src={userChoose[memberId].imageURL}
-              alt="img"
-              className={classes.members__image}
-            />
-            <div className="mt-2 mb-2">
+        <div className='d-flex'>
+          <div className='d-flex flex-column'>
+            <img src={userChoose[memberId].imageURL} alt='img' className={classes.members__image} />
+            <div className='mt-2 mb-2'>
               {userChoose[memberId].socialMedia.map(({ name, link }) => (
-                <a href={link} className="pe-2 " style={{ color: "#fff" }}>
-                  <i
-                    className={`social__media bi bi-${name}`}
-                    style={{ fontSize: "30px" }}
-                  />
+                <a href={link} className='pe-2 ' style={{ color: '#fff' }}>
+                  <i className={`social__media bi bi-${name}`} style={{ fontSize: '30px' }} />
                 </a>
               ))}
             </div>
           </div>
-          <div className="container d-flex flex-column">
+          <div className='container d-flex flex-column'>
             <span>
               <b>Страна:</b> {userChoose[memberId].county}
             </span>
@@ -58,9 +50,9 @@ const Members = () => {
               <b>Участие в данном проекте: </b>
               {userChoose[memberId].impact}
             </span>
-            <div className="d-flex">
+            <div className='d-flex'>
               <span>
-                <b>Стэк технологий:</b>{" "}
+                <b>Стэк технологий:</b>{' '}
               </span>
               {userChoose[[memberId]].stack.map(({ name }) => (
                 <span key={name}>{name}</span>
@@ -75,13 +67,13 @@ const Members = () => {
           </div>
         </div>
       </div>
-    );
+    )
   }
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Members</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Members;
+export default Members
